c8004b2dff65476c182c54ea30864c3ea3c7fc8d
downtracer <sharingan.uc@yandex.ru>
2020-06-28 17:56:32 +0300

    что-то там

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+//Задание 1
 
+let algebra = 3;
+let geography = 4;
+let physics = 5; 
+let mean = (algebra + geography + physics) / 3;
+
+console.log(mean);
+
+
+console.log();
+//Задание 2 
+let name = 'Алексей';
+let message = `Меня зовут ${name}`; 
+
+console.log(message);
+
+console.log();
